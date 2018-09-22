038adffef92ee949f840c9411e2ec26ded0b385b
Marcos Jacques Oliveira <marcosjacquesoliveira.1999@gmail.com>
2018-09-22 12:30:51 -0300

    subindo atividade-11

---
+++ atividade-11.js
@@ -0,0 +1,11 @@
+function ocultarTexto(){
+    document.getElementById('texto').style.display = "none";
+}
+
+function trocarcor(){
+    document.getElementById('texto2').style.color = 'blue';
+}
+
+function mostrartexto3(){
+    document.getElementById('texto3').style.display = "block"
+}
